6ce5d68462027cb0e019e8f4ced0cd433c0e817a
Victor Delgado <vdel26@gmail.com>
2017-02-09 00:36:54 +0100

    js starter

--- scripts/_source/main.js
+++ scripts/_source/main.js
@@ -1,1 +1,28 @@
-// fontfaceobserver, picturefill
+import FontFaceObserver from 'fontfaceobserver'
+import Picturefill from 'picturefill'
+
+const $ = document.querySelector.bind(document)
+
+class App {
+  constructor () {
+    this.init()
+    this.bindEvents()
+  }
+
+  bindEvents () {
+    $('.js-desktop-showcase').addEventListener('mouseover', this.desktopShowcase)
+  }
+
+  desktopShowcase (evt) {
+    console.log(evt.target)
+    // filter: target has data-id attr?
+    // show description for this one
+    // hide all others in the list
+  }
+
+  mobileShowcase () {}
+
+  init () {}
+}
+
+document.addEventListener('DOMContentLoaded', () => window.app = new App())
