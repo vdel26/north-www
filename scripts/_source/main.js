78f1ca2a3cd4d45c345d0a999dafaa36b8cb105c
Victor Delgado <victordg@Victors-MacBook-Pro.local>
2017-02-10 02:02:10 +0100

    desktop project showcase – first version

--- scripts/_source/main.js
+++ scripts/_source/main.js
@@ -10,17 +10,34 @@ class App {
   }
 
   bindEvents () {
-    $('.js-desktop-showcase').addEventListener('mouseover', this.desktopShowcase)
+    $('.js-desktop-showcase').addEventListener('mouseover', this.desktopShowcaseSet)
+    $('.js-desktop-showcase').addEventListener('mouseleave', this.desktopShowcaseLeave)
   }
 
-  desktopShowcase (evt) {
-    console.log(evt.target)
-    // filter: target has data-id attr?
-    // show description for this one
-    // hide all others in the list
+  desktopShowcaseSet (evt) {
+    // set to project image
+    console.log('over', evt.target.dataset.id)
+    const projectId = evt.target.dataset.id
+    const item = $(`.showcase__item[data-id=${projectId}]`)
+    if (item) {
+      item.classList.add('is-active')
+    }
   }
 
-  mobileShowcase () {}
+  desktopShowcaseLeave (evt) {
+    // set to black background
+    console.log('leave', evt.target)
+    const items = document.querySelectorAll('.showcase__item')
+    Array.from(items).map((item) => {
+      if (item.classList.contains('is-active')) {
+        item.classList.remove('is-active')
+      }
+    })
+  }
+
+  mobileShowcase () {
+    // rotate through projects list in 3s intervals
+  }
 
   init () {}
 }
